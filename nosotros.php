0234a30100a6fa521d5fd26f1a6e698c7aa4974f
Elida Ayala Guerrero <ayala2160@gmial.com>
2021-06-17 14:58:21 -0500

    Creacion del proyecto

---
+++ nosotros.php
@@ -0,0 +1,8 @@
+<?php include("template/cabecera.php");?>
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Hola que hace</p>
+    <hr class="my-2">
+          
+</div>
+<?php include("template/pie.php");?>
---
+++ productos.php
@@ -0,0 +1,53 @@
+<?php include("template/cabecera.php");?>
+
+<div class="col-md-3">
+    
+</div>
+
+<div class="card">
+
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+
+</div>
+
+
+<div class="card">
+
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+
+</div>
+
+
+<div class="card">
+
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+
+</div>
+
+
+<div class="card">
+
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+    </div>
+
+</div>
+<?php include("template/pie.php");?>
