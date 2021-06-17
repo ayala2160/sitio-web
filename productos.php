cb758b9d5f4e1975e8d89eed4133fc844bdacbf8
Elida Ayala Guerrero <ayala2160@gmial.com>
2021-06-17 18:46:39 -0500

    Estructura del administrador

--- productos.php
+++ productos.php
@@ -1,53 +1,60 @@
 <?php include("template/cabecera.php");?>
 
 <div class="col-md-3">
+
+    <div class="card">
+
+        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+        <div class="card-body">
+            <h4 class="card-title">Libro php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
+
+    </div>
     
 </div>
 
-<div class="card">
+<div class="col-md-3">
 
-    <img class="card-img-top" src="holder.js/100x180/" alt="">
+    <div class="card">
 
-    <div class="card-body">
-        <h4 class="card-title">Title</h4>
-        <p class="card-text">Text</p>
+        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+        <div class="card-body">
+            <h4 class="card-title">Libro php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
+
     </div>
-
+    
 </div>
+<div class="col-md-3">
 
+    <div class="card">
 
-<div class="card">
+        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
 
-    <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
 
-    <div class="card-body">
-        <h4 class="card-title">Title</h4>
-        <p class="card-text">Text</p>
     </div>
-
+    
 </div>
+<div class="col-md-3">
 
+    <div class="card">
 
-<div class="card">
+        <img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
 
-    <img class="card-img-top" src="holder.js/100x180/" alt="">
+        <div class="card-body">
+            <h4 class="card-title">Libro php</h4>
+            <a name="" id="" class="btn btn-primary" href="#" role="button">Ver más</a>
+        </div>
 
-    <div class="card-body">
-        <h4 class="card-title">Title</h4>
-        <p class="card-text">Text</p>
     </div>
-
-</div>
-
-
-<div class="card">
-
-    <img class="card-img-top" src="holder.js/100x180/" alt="">
-
-    <div class="card-body">
-        <h4 class="card-title">Title</h4>
-        <p class="card-text">Text</p>
-    </div>
-
+    
 </div>
 <?php include("template/pie.php");?>
--- template/cabecera.php
+++ template/cabecera.php
@@ -34,4 +34,5 @@
     </nav>
 
 <div class="container">
+<br>
     <div class="row">
